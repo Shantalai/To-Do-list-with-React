ecc056abca1ade56c3c6cc44b8f93262280c0426
Anastasia Kaliakova <omkalimata09@gmail.com>
2020-07-15 09:27:27 -0400

    Main app component

---
+++ App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import './App.css';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
+class App extends Component {
+  state = {
+    users:[
+      {userName: 'not Ana'},
+      {userName: 'not Mike'},
+      {userName: 'not Oly'}
+    ]
+  }
+
+  switchNameHandler = () => {
+    // NOT CORRECT: this.state.users[0] = "Ana Ana Ana!";
+    this.setState({
+      users: [
+        {userName: 'Ana Ana Ana!'},
+        {userName: 'Mikey Mikey'},
+        {userName: 'Oly Oly!'}
+      ]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ol>
+          <li>Create TWO new components: UserInput and UserOutput</li>
+          <li>UserInput should hold an input element, UserOutput two paragraphs</li>
+          <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
+          <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
+          <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
+          <li>Add a method to manipulate the state (=> an event-handler method)</li>
+          <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
+          <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
+          <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
+          <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
+        </ol>
+      <button onClick={this.switchNameHandler}>Switch user name</button>
+
+      <UserOutput userName={this.state.users[0].userName}/>
+      <UserInput />
+      <UserOutput userName={this.state.users[1].userName}/>
+      <UserInput />
+      <UserOutput userName={this.state.users[2].userName}/>
+      <UserInput />
+      </div>
+    );
+  }
+}
+
+export default App;
